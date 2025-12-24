3c7cde67e6ae7b7fcca64952e1d7f1679594d63d
aditya-nehare <adityanehare571@gmail.com>
2025-12-24 10:00:17 +0530

    Design Basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,24 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>ToDo LIST</h1>
-    </div>
+    <main>
+      <h1>ToDo List</h1>
+      <div className="container">
+        <input type="text" />
+        <button>Add Task</button>
+      </div>
+
+      <div className="list-container">
+        <div className="todo">
+          <p>Go to the GYM</p>
+
+          <div className="actions">
+            <input type="checkbox" />
+            <button>Delete</button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
